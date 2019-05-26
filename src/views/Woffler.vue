f8e584c49796238d8f6a51b9720b19ddbfb8c86e
Konstantin Krupovich <kkrupovich@gmail.com>
2019-05-26 16:34:30 +0300

    #6 Add Vuetify framework and create some layout: content moved to default layout;

--- src/views/Woffler.vue
+++ src/views/Woffler.vue
@@ -1,39 +1,21 @@
 <template>
-  <div class="outer-panel flex-column">
-    <div class="inner-panel">
-      <v-card>
-        <v-toolbar :color="bar.class" :dark="bar.dark">
-          <v-toolbar-side-icon></v-toolbar-side-icon>
-          <v-toolbar-title>Woffler game</v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-btn icon>
-            <v-icon>favorite</v-icon>
-          </v-btn>
-        </v-toolbar>
-        <v-card-text>
-          <div class="wflbox">&nbsp;</div>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn icon>
-            <v-icon>favorite</v-icon>
-          </v-btn>
-        </v-card-actions>        
-      </v-card>
-    </div>
-  </div>
+  <v-card>
+    <v-card-text>
+      <div class="wflbox">&nbsp;</div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn icon>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+    </v-card-actions>        
+  </v-card>
 </template>
 <script>
 
   export default {
     components: {},
     data: function() {
-      return {
-        bar: 
-          // { 'class': '' },
-           { 'class': '', dark: true },
-          // { 'class': 'primary', dark: true },
-          // { 'class': 'elevation-0' }        
-      };
+      return {};
     },
     created: function() {
     },
